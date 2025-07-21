ac8f56843bc679561f7150248d10d34b1eca1971
Sunil-Harde <158262002+Sunil-Harde@users.noreply.github.com>
2025-07-21 17:56:23 +0530

    nav done

--- client/src/Components/NavBar/Navbar.jsx
+++ client/src/Components/NavBar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdContact } from "react-icons/io";
 import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
+
+    const [open, setOpen]=useState(true)
+
     const handelNavItems = () => {
-        console.log("hello")
+        setOpen(!open)
     }
     return (
         <div className=''>
@@ -13,7 +16,7 @@ function Navbar() {
                     <div className='container mx-auto flex justify-between items-center'>
 
                         <div className='text-black text-2xl font-bold'>Coffee Shop</div>
-                        <ul className=' space-x-4 block fixed md:relative right-0 top-20 md:top-0 bg-gray-800 min-h-screen md:min-h-10 md:mr-10 md:h-10 w-40 md:w-0 text-center md:flex '>
+                        <ul className={ `${open? 'hidden':'block'} space-x-4  fixed md:relative right-0 top-20 md:top-0 bg-gray-800 min-h-screen md:min-h-10 md:mr-10 md:h-10 w-40 md:w-0 text-center md:flex `}>
                             <li className='text-white hover:text-gray-400 cursor-pointer mt-20 md:mt-0'>Home</li>
                             <li className='text-white hover:text-gray-400 cursor-pointer mt-10 md:mt-0'>Menu</li>
                             <li className='text-white hover:text-gray-400 cursor-pointer mt-10 md:mt-0' >About</li>
@@ -22,7 +25,7 @@ function Navbar() {
                         </ul>
                         <div className='flex items-center space-x-4'>
                             <IoMdContact className="fas fa-home text-2xl text-white" />
-                            <IoMenu className='md:hidden' onClick={handelNavItems} />
+                            <IoMenu className='md:hidden text-white' onClick={handelNavItems}  />
                         </div>
                     </div>
                 </nav>
