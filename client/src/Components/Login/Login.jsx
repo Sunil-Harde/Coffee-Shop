7bf3dda58af6172838eadc3e462e6083c5872f55
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-07-21 17:37:14 +0530

    nav created

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -7,28 +7,28 @@ function Login() {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
-
-    const handalSubmit = (e)=>{
+    const handalSubmit = (e) => {
         e.preventDefault();
         console.log(username);
         console.log(password);
         console.log(email);
-        
+
     }
 
     return (
         <div className=''>
             <Navbar />
-            <div className='container mx-auto p-4 bg-white shadow-md rounded-lg mt-10'>
+            <div className='container mx-auto p-4 bg-red-200 shadow-md rounded-lg mt-10'>
 
                 <h1 className="text-2xl font-bold mb-4">Login</h1>
                 <p className="mb-4">Please enter your credentials to login.</p>
                 <form className=''>
-                    <input type="text" placeholder="Username" className="border p-2 rounded mb-4 w-full" onChange={(e)=>setUsername(e.target.value)}/>
-                    <input type="password" placeholder="Password" className="border p-2 rounded mb-4 w-full" onChange={(e)=>setPassword(e.target.value)}/>
-                    <input type="email" placeholder="email" className="border p-2 rounded mb-4 w-full" onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="text" placeholder="Username" className="border p-2 rounded mb-4 w-full" onChange={(e) => setUsername(e.target.value)} />
+                    <input type="password" placeholder="Password" className="border p-2 rounded mb-4 w-full" onChange={(e) => setPassword(e.target.value)} />
+                    <input type="email" placeholder="email" className="border p-2 rounded mb-4 w-full" onChange={(e) => setEmail(e.target.value)} />
                     <button type="" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={handalSubmit}>Login</button>
                 </form>
+
             </div>
         </div>
     )
--- client/src/Components/NavBar/Navbar.jsx
+++ client/src/Components/NavBar/Navbar.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
+import { IoMdContact } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
+    const handelNavItems = () => {
+        console.log("hello")
+    }
     return (
-        <div>
-            <div className='bg-gray-100 '>
-
-                <nav className='bg-gray-800 p-4 h-20'>
+        <div className=''>
+            <div className=' bg-gray-100 '>
+                <nav className=' bg-gray-800 p-4 h-20'>
                     <div className='container mx-auto flex justify-between items-center'>
                         <div className='text-white text-2xl font-bold'>Coffee Shop</div>
-                        <ul className='flex space-x-4'>
+                        <ul className='  space-x-4 gap-20 md:flex hidden'>
                             <li className='text-white hover:text-gray-400 cursor-pointer'>Home</li>
                             <li className='text-white hover:text-gray-400 cursor-pointer'>Menu</li>
                             <li className='text-white hover:text-gray-400 cursor-pointer'>About Us</li>
                             <li className='text-white hover:text-gray-400 cursor-pointer'>Contact</li>
                         </ul>
-
                         <div className='flex items-center space-x-4'>
-                            <button className='bg-white text-gray-800 px-4 py-2 rounded hover:bg-gray-200 cursor-pointer'>Login</button>
-                            <button className='bg-white text-gray-800 px-4 py-2 rounded hover:bg-gray-200 cursor-pointer'>Sign Up</button>
+                            <IoMdContact className="fas fa-home text-2xl text-white" />
+                            <IoMenu className='md:hidden' onClick={handelNavItems} />
                         </div>
                     </div>
-
-
                 </nav>
-
 
             </div>
         </div>
