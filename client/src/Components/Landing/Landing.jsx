99c1765bd116ca61291d9baf452ec8fff461ac01
Sunil-Harde <158262002+Sunil-Harde@users.noreply.github.com>
2025-07-29 15:44:42 +0530

    some animetion added

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -10,84 +10,64 @@ import { motion, scale } from 'motion/react'
 
 function Landing() {
     return (
-        <div className='h-screen w-screen'>
-            <Navbar />
-            <div className='grid  grid-cols-2'>
+        <>
+            <div className='h-screen w-screen bg-red-600'>
+                <Navbar />
+                <div className='grid  grid-cols-2'>
 
-                <div className='min-h-screen relative bg-gray-900 flex items-center justify-center'>
+                    <div className='h-screen relative bg-gray-900 flex items-center justify-center '>
 
-                    <motion.h1 className='text-amber-50 text-5xl w-10 -mt-30 -ml-30 font-bold ' initial={{ x: -200 }} animate={{ x: 0 }} transition={
-                        {
-                            delay: .3,
-                            duration: 1
-                        }
-                    }>
-                        <span className='text-amber-500 '>Chocolate</span> Latte
-                    </motion.h1>
+                        <motion.h1 className='text-amber-50 sm:text-5xl md:text-5xl w-10 mt-0 md:-mt-30 -ml-50 md:-ml-30  font-bold hidden sm:block ' initial={{ x: -200 }} animate={{ x: 0 }} transition={{ delay: .3, duration: 1 }}>
+                            <span className='text-amber-500 mr-2'>Chocolate</span> Latte
+                        </motion.h1>
 
-                    <div className='w-40 h-40 bg-black absolute bottom-10 left-10 ' >
-                        <motion.h1 className='text-gray-500 w-60 ml-15 -mt-4 tracking-wide font-bold text-2xl ' initial={{ x: -200 }} animate={{ x: 0 }} transition={
-                            {
-                                delay: .3,
-                                duration: 1
-                            }
-                        }><span className='text-amber-500 '>Coffee</span> Lovers</motion.h1>
-                        <motion.p className='text-gray-500 w-60 ml-15 mt-2 tracking-wide' initial={{ x: -220 }} animate={{ x: 0 }} transition={
-                            {
-                                delay: .3,
-                                duration: 1.2
-                            }
-                        }>Your daily cup of coffee gets even better in our Plant-Based..  Ditch the dairy with our Plant-Based Almond special, paired ..</motion.p>
+                        <div className='w-40 h-40 bg-black absolute bottom-10 left-10  hidden md:block ' >
+                            <motion.h1 className='text-gray-500 w-60 ml-15 -mt-4 tracking-wide font-bold text-2xl ' initial={{ x: -200 }} animate={{ x: 0 }} transition={{ delay: .3, duration: 1 }}><span className='text-amber-500 '>Coffee</span> Lovers</motion.h1>
+                            <motion.p className='text-gray-500 w-60 ml-15 mt-2 tracking-wide' initial={{ x: -220 }} animate={{ x: 0 }} transition={{ delay: .3, duration: 1.2 }}>Your daily cup of coffee gets even better in our Plant-Based..  Ditch the dairy with our Plant-Based Almond special, paired ..</motion.p>
+                        </div>
                     </div>
-                </div>
 
 
-                <div className=' relative min-h-full flex items-center justify-center bg-black'>
+                    <div className=' relative min-h-full flex items-center justify-center bg-black overflow-x-hidden'>
 
-                    <motion.h1 className='text-amber-50 text-5xl w-10 -mt-30 -ml-30 font-bold' initial={{ x: -200 }} animate={{ x: 0 }} transition={
-                        {
-                            delay: .3,
-                            duration: 1
-                        }
-                    }>
-                        <span className='text-amber-500 '>Chocolate</span> Latte
-                    </motion.h1>
+                        <motion.h1 className='text-amber-50 text-5xl w-10 sm:-mt-40 mt-0 -ml-30 font-bold hidden md:block' initial={{ x: 200 }} animate={{ x: 0 }} transition={{ delay: .3, duration: 1 }}>
+                            <span className='text-amber-500 '>Chocolate</span> Latte
+                        </motion.h1>
 
 
-                    <div className='w-40 h-40 bg-gray-900 absolute bottom-10 right-10 ' >
-                        <motion.h1 className='text-gray-500 w-60 absolute -top-5 -left-30 tracking-wide font-bold text-2xl ' initial={{ x: -200 }} animate={{ x: 0 }} transition={
-                            {
-                                delay: .3,
-                                duration: 1
-                            }
-                        }><span className='text-amber-500 '>Coffee</span> Lovers</motion.h1>
-                        <motion.p className='text-gray-500 w-60 absolute -left-30 top-5 tracking-wide' initial={{ x: -220 }} animate={{ x: 0 }} transition={
-                            {
-                                delay: .3,
-                                duration: 1.2
-                            }
-                        }>Your daily cup of coffee gets even better in our Plant-Based..  Ditch the dairy with our Plant-Based Almond special, paired ..</motion.p>
+                        <div className='w-40 md:h-40 bg-gray-900 absolute -mt-12  md:bottom-10 sm:right-0 md:right-10 sm:block hidden ' >
+                            <motion.h1 className='text-gray-500 w-60 absolute -top-5 -left-25 tracking-wide font-bold text-2xl ' initial={{ x: 200 }} animate={{ x: 0 }} transition={{ delay: .3, duration: 1 }}><span className='text-amber-500 '>Coffee</span> Lovers</motion.h1>
+                            <motion.p className='text-gray-500 w-60 absolute -left-25 top-5 tracking-wide' initial={{ x: 220 }} animate={{ x: 0 }} transition={{ delay: .3, duration: 1.2 }}>Your daily cup of coffee gets even better in our Plant-Based..  Ditch the dairy with our Plant-Based Almond special, paired ..</motion.p>
+                        </div>
 
                     </div>
 
                 </div>
 
-            </div>
 
+                <div className=''>
+                    <div className=' flex justify-center items-center ' >
+                        <motion.img src={BlackCoffee} alt="" style={{ transition: 'transform 0.1s ease' }} className=' h-60 sm:h-70 md:h-80 w-35 sm:w-40 md:w-50 top-35 md:top-50 absolute' whileHover={{ scale: 1.1, transition: { delay: 0, duration: .1 } }} whileTap={{ scale: 1, transition: { delay: 0, duration: .1 } }} initial={{ opacity: 0, scale: 0.7 }} animate={{ opacity: 1, scale: 1, transition: { duration: 0.5 } }} />
+                    </div>
+                </div>
 
-            <div className=''>
+                <div>
+                    <div className=' flex justify-center items-center sm:hidden '>
 
-                <div className=' flex justify-center items-center ' initial={{ scale: 7 }} animate={{ scale: 1 }} transition={
-                    {
-                        delay: .3,
-                        duration: 1
-                    }}>
-                    <motion.img src={BlackCoffee} alt="" className='h-80 w-50 top-50 absolute' whileHover={{ scale: 1.1 }} whileTap={{ scale: 1 }} />
+                        <motion.h1 className='absolute text-amber-50 z-100 text-2xl font-bold top-20' initial={{ y: -50, opacity:0 }} animate={{ y: 0, opacity:1 }} transition={{ delay: .3, duration: 1 }}>
+                            <span className='text-amber-500 '>Chocolate</span> Latte
+                        </motion.h1>
+
+                        <div className='z-100 -mt-20 absolute bottom-5 flex justify-center items-center flex-col ' >
+                            <motion.h1 className='text-gray-500 w-60 ml-15 -mt-4 tracking-wide font-bold text-2xl ' initial={{ y: 50, opacity:0 }} animate={{ y: 0, opacity:1 }} transition={{ delay: .3, duration: 1 }}><span className='text-amber-500 '>Coffee</span> Lovers</motion.h1>
+                            <motion.p className='text-gray-500 w-60 ml-15 mt-2 tracking-wide' initial={{ y: 52, opacity:0 }} animate={{ y: 0, opacity:1 }} transition={{ delay: .3, duration: 1.2 }}>Your daily cup of coffee gets even better in our Plant-Based..  Ditch the dairy with our Plant-Based Almond special, paired ..</motion.p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
+        </>
 
-        </div>
     )
 }
 
--- client/src/Components/NavBar/Navbar.jsx
+++ client/src/Components/NavBar/Navbar.jsx
@@ -8,6 +8,7 @@ import { motion } from 'motion/react';
 
 
 function Navbar() {
+
     const [openNav, setOpenNav] = useState(true)
     const [showLogin, setShowLogin] = useState(false)
     const [navColor, setNavColor] = useState(false)
@@ -28,6 +29,8 @@ function Navbar() {
         setShowLogin(!showLogin)
     }
 
+    const navItems = ["Home", "Menu", "About", "Contact"];
+
 
     return (
         <div className=' fixed z-10 w-full'>
@@ -36,15 +39,18 @@ function Navbar() {
                     <div className='container mx-auto flex justify-between items-center h-15 transition-all duration-900 ease-in-out'>
 
                         <motion.div className=' text-2xl font-bold font-mono' initial={{ y: -40 }} animate={{ y: 0 }} transition={{ delay: .3, duration: 1 }}><span className='text-amber-500'>Coffee</span> Shop</motion.div>
-                        <ul className={`${openNav ? " bg-amber-50 text-amber-30 -mt-400 " : ''} md:mt-0 block z-10 gap-10 md:gap-5 space-x-4 fixed md:relative right-0 top-15 md:top-2 bg-gray-800 min-h-screen md:min-h-10 md:mr-10 md:h-10 w-40 md:w-0 text-center md:flex transition-all duration-400 ease-in-out`}>
-                            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className=' hover:text-gray-400 cursor-pointer mt-10 md:mt-0' initial={{ y: -50 }} animate={{ y: 0 }} transition={{ delay: .3, duration: 1.2 }}>Home</motion.li>
-                            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className=' hover:text-gray-400 cursor-pointer mt-10 md:mt-0' initial={{ y: -53 }} animate={{ y: 0 }} transition={{ delay: .3, duration: 1.4 }}>Menu</motion.li>
-                            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className=' hover:text-gray-400 cursor-pointer mt-10 md:mt-0' initial={{ y: -56 }} animate={{ y: 0 }} transition={{ delay: .3, duration: 1.6 }}>About</motion.li>
-                            <motion.li whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className=' hover:text-gray-400 cursor-pointer mt-10 md:mt-0' initial={{ y: -59 }} animate={{ y: 0 }} transition={{ delay: .3, duration: 1.8 }}>Contact</motion.li>
+                        <ul className={`${openNav ? "bg-amber-50 text-amber-30 -mt-400 " : ''} md:mt-0 block z-10 gap-10 md:gap-5 space-x-4 fixed md:relative right-0 top-15 md:top-2 bg-gray-800 min-h-screen md:min-h-10 md:mr-10 md:h-10 w-40 md:w-0 text-center md:flex transition-all duration-400 ease-in-out`}>
+
+                            {navItems.map((item, index) => (
+                                <motion.li key={item} className='hover:text-amber-400 cursor-pointer mt-10 md:mt-0' initial={{ y: -50 - index * 3 }} animate={{ y: 0 }} transition={{ y: { delay: 0.3, duration: 0.6 + index * 0.2 } }} whileHover={{ scale: 1.1, transition: { type: "spring", duration: 0.3 } }} whileTap={{ scale: 0.9 }}> {item} </motion.li>
+                            ))}
+
                         </ul>
-                        <motion.div className='flex items-center space-x-4 z-10' whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} initial={{ y: -62 }} animate={{ y: 0 }} transition={{ delay: .3, duration: 2 }}>
-                            <IoMdContact className="fas fa-home text-2xl " onClick={handelLoginPage} />
-                            <IoMenu className='md:hidden ' onClick={handelNavItems} />
+                        <motion.div className='flex items-center space-x-4 z-10 '>
+                            <motion.div className="fas fa-home text-2xl  cursor-pointer hover:text-amber-400" onClick={handelLoginPage} whileHover={{ scale: 1.1, transition: { type: "spring", duration: 0.3 } }} whileTap={{ scale: 0.9 }} initial={{ y: -62 }} animate={{ y: 0 }} transition={{ y:{delay: .3, duration: 2} }}>
+                                <IoMdContact   />
+                            </motion.div>
+                            <IoMenu className='md:hidden hover:text-amber-400 cursor-pointer' onClick={handelNavItems} />
                         </motion.div>
                     </div>
                 </nav>
